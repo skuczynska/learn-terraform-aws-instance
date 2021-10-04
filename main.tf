1188548340f39f8d0c5d6d940d30230ff2f44bde
skuczynska <skuczynska@pgs-soft.com>
2021-10-04 11:11:15 +0200

    Removed history

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  backend "remote" {
+    organization = "hashicorp-learn"
+    workspaces {
+      name = "Example-Worspace"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-central-1"
+}
+
+resource "aws_instance" "skuczynska-app-server" {
+  ami           = "ami-05f7491af5eef733a"
+  instance_type = "t3.micro"
+
+  tags = {
+    Owner = "skuczynska"
+    Name = var.instance_name
+  }
+}
